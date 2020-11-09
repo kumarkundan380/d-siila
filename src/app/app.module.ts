cb05bf079dfa5248a26f01646efd39c7f1a1ecfe
kumarkundan380 <kumarkundan380@gmail.com>
2020-11-09 15:28:05 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { SiilafoundationComponent } from './siilafoundation/siilafoundation.component';
+import { TeamComponent } from './team/team.component';
+import { TermsComponent } from './terms/terms.component';
+import { BlankComponent } from './layout/blank/blank.component';
+import { FullComponent } from './layout/full/full.component';
+import { PartialComponent } from './layout/partial/partial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FaqComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    JobsComponent,
+    SiilafoundationComponent,
+    TeamComponent,
+    TermsComponent,
+    BlankComponent,
+    FullComponent,
+    PartialComponent
   ],
   imports: [
     BrowserModule,
